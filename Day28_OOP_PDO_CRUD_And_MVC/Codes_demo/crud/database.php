815fef037c8594860d623fea7ddf787b7eb52da0
Manh <nvmanh@cmc.com.vn>
2021-01-27 18:56:12 +0700

    a

---
+++ Day28_OOP_PDO_CRUD_And_MVC/Codes_demo/crud/database.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * crud/database.php
+ * CRUD qly sách với PDO
+ * - CSDL: php1020e_pdo, bảng books:id,name,
+ * amount,created_at
+ CREATE DATABASE IF NOT EXISTS php1020e_pdo
+ * - crud/database.php
+ *     /create.php
+ *     /update.php
+ *     /detail.php
+ *     /delete.php
+ *     /index.php
+ */
