ab092c318325d684bf519b71b1d0d096733c3b7f
Manh <nvmanh@cmc.com.vn>
2021-04-07 19:14:01 +0700

    A

--- Day35_Demo_Project_Part_4_Login_Register/mvc_project/backend/controllers/UserController.php
+++ Day35_Demo_Project_Part_4_Login_Register/mvc_project/backend/controllers/UserController.php
@@ -2,5 +2,18 @@
 require_once 'controllers/Controller.php';
 
 class UserController extends Controller {
+  //Phương thức đăng ký tài khoản
+  // index.php?controller=user&action=register
+  public function register() {
+    // + Lấy nội dung view
+    $this->content =
+    $this->render('views/users/register.php');
+    // + Gọi layout để hiển thị view
+    // Do chức năng đăng ký dùng cho các user chưa
+    //login,nên ko thể gọi layout main đc, nên
+    //cần tạo layout mới -> copy file layout chính,
+    //loại bỏ các phần ko cần thiết
+    require_once 'views/layouts/main_login.php';
+  }
 
 }
